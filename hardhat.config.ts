089e0164cf170a77e224a819b41eba3014aca9b1
madhurMongia <madhurrajput1010@gmail.com>
2025-05-13 03:14:37 +0530

    feat: deploy POH Circles Proxy contract with config and deployment scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
 import * as dotenv from "dotenv";
 
 dotenv.config();
@@ -21,6 +22,16 @@ const config: HardhatUserConfig = {
     localhost: {
       url: "http://127.0.0.1:8545",
     },
+    chiado: {
+      chainId: 10200,
+      url: process.env.CHIADO_RPC_URL || "https://rpc.chiado.gnosis.gateway.fm",
+      accounts: [process.env.PRIVATE_KEY!]
+    },
+    gnosis: {
+      chainId: 100,
+      url: process.env.GNOSIS_RPC_URL || "https://rpc.gnosischain.com",
+      accounts: [process.env.PRIVATE_KEY!]
+    },
   },
   paths: {
     sources: "./contracts",
@@ -29,8 +40,31 @@ const config: HardhatUserConfig = {
     artifacts: "./artifacts",
   },
   mocha: {
-    timeout: 40000,
+    timeout: 1000000000,
   },
-};
+  etherscan: {
+    apiKey: {
+      gnosis: process.env.BLOCKSCOUT_API_KEY || "any-string-for-blockscout",
+      chiado: process.env.BLOCKSCOUT_API_KEY || "any-string-for-blockscout"
+    },
+    customChains: [
+      {
+        network: "gnosis",
+        chainId: 100,
+        urls: {
+          apiURL: "https://gnosis.blockscout.com/api",
+          browserURL: "https://gnosis.blockscout.com"
+        }
+      },
+      {
+        network: "chiado",
+        chainId: 10200,
+        urls: {
+          apiURL: "https://gnosis-chiado.blockscout.com/api",
+          browserURL: "https://gnosis-chiado.blockscout.com"
+        }
+      }
+    ]
+  }};
 
 export default config;
